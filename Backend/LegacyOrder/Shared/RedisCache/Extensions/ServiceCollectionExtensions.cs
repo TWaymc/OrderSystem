543df20868d810edcc19078e6afb2e4a89a20cb2
TWay <twaymc@gmail.com>
2026-04-15 08:25:05 +0200

    Add Redis Shared AppService
    a very generic library for cache

---
+++ Backend/LegacyOrder/Shared/RedisCache/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RedisCache.Service;
+using StackExchange.Redis;
+
+namespace RedisCache.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRedisService(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Redis") 
+                               ?? throw new InvalidOperationException("Redis connection string not configured.");
+
+        services.AddSingleton<IConnectionMultiplexer>(
+            ConnectionMultiplexer.Connect(connectionString));
+
+        services.AddSingleton<IRedisCacheService, RedisCacheService>();
+
+        return services;
+    }
+}
---
+++ Backend/LegacyOrder/Shared/RedisCache/Service/RedisCacheService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using StackExchange.Redis;
+
+namespace RedisCache.Service;
+
+// very generic cache Service 
+
+public class RedisCacheService: IRedisCacheService
+{
+    private readonly IDatabase _db;
+
+    public RedisCacheService(IConnectionMultiplexer redis)
+    {
+        _db = redis.GetDatabase();
+    }
+
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        var value = await _db.StringGetAsync(key);
+        return value.IsNull ? default : JsonSerializer.Deserialize<T>(value!);
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+    {
+        var json = JsonSerializer.Serialize(value);
+        await _db.StringSetAsync(
+            key: key,
+            value: (RedisValue)json,
+            expiry: expiry,
+            when: When.Always,
+            flags: CommandFlags.None);
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await _db.KeyDeleteAsync(key);
+    }
+}
